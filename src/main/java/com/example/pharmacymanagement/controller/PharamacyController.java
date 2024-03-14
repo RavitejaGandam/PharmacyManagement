393172da85355618bca6f288b0707bfe7a89f9fe
raviteja gandam <gandamraviteja@gmail.com>
2024-03-14 15:25:04 +0530

    full dev completed

--- src/main/java/com/example/pharmacymanagement/controller/PharamacyController.java
+++ src/main/java/com/example/pharmacymanagement/controller/PharamacyController.java
@@ -1,9 +1,12 @@
 package com.example.pharmacymanagement.controller;
 
+import com.example.pharmacymanagement.entity.PharmacyEntity;
 import com.example.pharmacymanagement.service.PharamacyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("/pharamacyDetails")
@@ -11,4 +14,26 @@ public class PharamacyController {
 
     @Autowired
     PharamacyService service;
+
+    @GetMapping("/allDrugs")
+   public ResponseEntity<List<PharmacyEntity>> getAllDrugs(){
+       List<PharmacyEntity> pharmacy = service.getAll();
+       return ResponseEntity.ok(pharmacy);
+   }
+
+   @PostMapping("/addDrug")
+   public PharmacyEntity addDrug(@RequestBody PharmacyEntity pharmacy){
+        return service.createDrug(pharmacy);
+   }
+   @PutMapping("/updateDrug{id}")
+   public PharmacyEntity updateDrug(@PathVariable Long id, @RequestBody PharmacyEntity pharmacy){
+        return service.updateDrug(id,pharmacy);
+   }
+
+   @DeleteMapping("/deleteDrug{id}")
+   public ResponseEntity<Void> deleteDrug(@PathVariable Long id){
+        service.deleteDrug(id);
+        return ResponseEntity.noContent().build();
+   }
+
 }
